ab37ebd194c3602488b42a5ef0b62493abd8bf17
oneight2 <47237509+oneight2@users.noreply.github.com>
2021-08-27 07:02:59 +0700

    delete + update

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator");
 const BlogPost = require("../models/blog");
+const path = require("path");
+const fs = require("fs");
 
 exports.createBlogPost = (req, res, next) => {
   // cek ada error ga
@@ -74,4 +76,85 @@ exports.getBlogPostById = (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+};
+
+exports.updateBlogPost = (req, res, next) => {
+  // cek ada error ga
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error("Invalid Input");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+  if (!req.file) {
+    const err = new Error("Image Tidak Ada");
+    err.errorStatus = 422;
+    err.data = errors.array();
+    throw err;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const image = req.file.path;
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Data Tidak Ditemukan");
+        err.errorStatus = 404;
+        throw err;
+      }
+      post.title = title;
+      post.content = content;
+      post.image = image;
+
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Data Berhasil diupdate",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.deleteBlogPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Data Tidak Ditemukan");
+        err.errorStatus = 404;
+        throw err;
+      }
+
+      removeImage(post.image);
+
+      return BlogPost.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Data Berhasil dihapus",
+        data: result,
+      });
+    })
+
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const removeImage = (filePath) => {
+  console.log("filepath", filePath);
+  console.log("dirname", __dirname);
+
+  // alamatnya digabung __dirname keluar 2 kali terus ke filepath
+  filePath = path.join(__dirname, "../..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const {body} = require('express-validator')
+const { body } = require("express-validator");
 
 const blogController = require("../controller/blog");
 
 // [POST]: v1/blog/post
-router.post("/post",[
-    body('title').isLength({min:5}).withMessage('Title Input Invalid'),
-    body('content').isLength({min:5}).withMessage('Content Input Invalid')
-
-], blogController.createBlogPost);
+router.post(
+  "/post",
+  [
+    body("title").isLength({ min: 5 }).withMessage("Title Input Invalid"),
+    body("content").isLength({ min: 5 }).withMessage("Content Input Invalid"),
+  ],
+  blogController.createBlogPost
+);
 
 router.get("/posts", blogController.getAllPosts);
 
 router.get("/post/:postId", blogController.getBlogPostById);
+router.put(
+  "/post/:postId",
+  [
+    body("title").isLength({ min: 5 }).withMessage("Title Input Invalid"),
+    body("content").isLength({ min: 5 }).withMessage("Content Input Invalid"),
+  ],
+  blogController.updateBlogPost
+);
+
+router.delete("/post/:postId", blogController.deleteBlogPost);
 
 module.exports = router;
